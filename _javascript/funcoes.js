39ec1ab8e7ba6b33a870305d8f5412ba506889ab
Geova Jonnatha <geova_jonnatha@hotmail.com>
2021-03-28 17:29:00 -0300

    updates

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,13 +1,35 @@
-let imagem_fixada = document.getElementById("icone").src;
-function mudaFoto(src, li) {
-  document.getElementById("icone").src = src;
-  li.addEventListener("mouseout", () => {
-    document.getElementById("icone").src = imagem_fixada;
-  });
+window.onload = () => {
+  let imagem_fixada = document.getElementById("icone").src;
+  function mudaFoto(src, li) {
+    document.getElementById("icone").src = src;
+    li.addEventListener("mouseout", () => {
+      document.getElementById("icone").src = imagem_fixada;
+    });
+  }
+  if (localStorage.getItem("modo") == null) {
+    localStorage.setItem("modo", "claro");
+  }
+  if (localStorage.getItem("modo") == "escuro") {
+    document.getElementById("modo").href = "./_css/night_style.css";
+  } else {
+    document.getElementById("modo").href = "./_css/estilo.css";
+  }
+};
+
+function calc_total() {
+  let nValor = Number(document.getElementById("cQtd").value);
+
+  document.getElementById("cTot").value = nValor * 1500;
 }
 
-function calc_total(){
-  let nValor = Number(document.getElementById('cQtd').value)
-
-  document.getElementById('cTot').value = nValor * 1500
+function modo() {
+  if (localStorage.getItem("modo") == "claro") {
+    document.getElementById("modo").href = "./_css/night_style.css";
+    localStorage.setItem("modo", "escuro");
+    document.getElementById('button_alter').innerText = 'Modo Normal'
+  } else {
+    document.getElementById("modo").href = "./_css/estilo.css";
+    localStorage.setItem("modo", "claro");
+    document.getElementById('button_alter').innerText = 'Modo Noturno'
+  }
 }
