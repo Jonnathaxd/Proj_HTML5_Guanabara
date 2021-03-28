60cf1ac71765b8df3fc3361f4318055c849c4491
Geova Jonnatha <geova_jonnatha@hotmail.com>
2021-03-28 15:24:18 -0300

    updates

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,12 +1,13 @@
-let imagem_fixada = './_imagens/glass-oculos-preto-peq.png'
-function mudaFoto(src, li){
-
-    document.getElementById('icone').src = src
-    li.addEventListener('mouseout', ()=>{
-        document.getElementById('icone').src = imagem_fixada
-    })
-        
-    
+let imagem_fixada = document.getElementById("icone").src;
+function mudaFoto(src, li) {
+  document.getElementById("icone").src = src;
+  li.addEventListener("mouseout", () => {
+    document.getElementById("icone").src = imagem_fixada;
+  });
 }
 
+function calc_total(){
+  let nValor = Number(document.getElementById('cQtd').value)
 
+  document.getElementById('cTot').value = nValor * 1500
+}
